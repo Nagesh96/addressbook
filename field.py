adcdda560785719916f03bc12ff59592598761e6
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-06 16:24:01 +0530

    Update field.py

--- field.py
+++ field.py
@@ -1,7 +1,7 @@
 import os
 tickets_details = os.environ.get('JIRA_TICKETS')
 value = os.environ.get('artifacturl')
-print(f'Artifact URL:' value)
+print(f'Artifact URL:', value)
 jira_tickets = tickets_details.split(',')
 for issue in jira_tickets:
-  print(f'Jira_Tickets'issue)
+  print(f'Jira_Tickets:', issue)
