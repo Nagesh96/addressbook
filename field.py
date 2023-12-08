651ea50cefec3dd4b86b644d487ef3347bef73bc
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2023-12-08 18:36:17 +0530

    Update field.py

--- field.py
+++ field.py
@@ -1,5 +1,7 @@
 import os
 tickets_details = os.environ.get('JIRA_TICKETS')
+customfield_id = os.environ.get('CUSTOMFIELD_ID')
+print(customfield_id)
 field_value = os.environ.get('FIELD_VALUE')
 print(f'Artifact URL:', field_value)
 jira_tickets = tickets_details.split(',')
