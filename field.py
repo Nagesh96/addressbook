59ea8bf8477123d8c155af239c499eb9ce78533f
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2024-02-21 14:50:29 +0530

    Delete field.py

--- field.py
+++
@@ -1,9 +0,0 @@
-import os
-tickets_details = os.environ.get('JIRA_TICKETS')
-customfield_id = os.environ.get('CUSTOMFIELD_ID')
-print(f'Custom field_id:',customfield_id)
-field_value = os.environ.get('FIELD_VALUE')
-print(f'Artifact URL:', field_value)
-jira_tickets = tickets_details.split(',')
-for issue in jira_tickets:
-  print(f'Jira_Tickets:', issue)
